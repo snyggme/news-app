883c23d52679844fd387d267dd3cd2d489e7feec
snyggme <rcbabahin@gmail.com>
2018-11-09 19:55:21 +0300

    Updates

---
+++ precache-manifest.33ab9bd59cba3ae470ce53974de0d9d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a21cbe90fd31540fb0384792fbc4a86",
+    "url": "/news-app/static/media/user.7a21cbe9.svg"
+  },
+  {
+    "revision": "7ab8c59ebdcac33ccc2c72a0cd755099",
+    "url": "/news-app/static/media/coffee.7ab8c59e.svg"
+  },
+  {
+    "revision": "dd5b3b6b432921443a6d",
+    "url": "/news-app/static/js/runtime~main.dd5b3b6b.js"
+  },
+  {
+    "revision": "a3de966457004ddaee1c",
+    "url": "/news-app/static/js/main.a3de9664.chunk.js"
+  },
+  {
+    "revision": "f069baed367057e1fb38",
+    "url": "/news-app/static/js/1.f069baed.chunk.js"
+  },
+  {
+    "revision": "a3de966457004ddaee1c",
+    "url": "/news-app/static/css/main.abb51a48.chunk.css"
+  },
+  {
+    "revision": "336ba8fb1a63a573790f561638d133cb",
+    "url": "/news-app/index.html"
+  }
+];
